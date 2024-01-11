9e749651ec8e761095baa37f0c97f3afbf417e71
Elisaudo <elisaudos@gmail.com>
2024-01-11 11:35:33 -0300

    atualização github

--- template-html/script.js
+++ template-html/script.js
@@ -1,11 +1,2 @@
-function testFunc() {
-    
-   // let a = document.getElementById('name');
-   // a.value = a.value.toUpperCase();
-
-   document.getElementById('name').value = document.getElementById('name').value.toUpperCase();
-    
-}
-
 
 
